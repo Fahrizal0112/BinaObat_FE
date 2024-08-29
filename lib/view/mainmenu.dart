58d78f4b92cbaf5b79cc0761730f7923acecef97
fahrizal <muchamad.fachrizal12@gmail.com>
2024-08-29 21:20:29 +0700

    finishing

--- lib/view/mainmenu.dart
+++ lib/view/mainmenu.dart
@@ -2,9 +2,12 @@ import 'package:bina_dokter/Signin/Signup/signin.dart';
 import 'package:bina_dokter/service/api_service.dart';
 import 'package:bina_dokter/view/pharmacypage.dart';
 import 'package:bina_dokter/view/prescriptionDetailsPage.dart';
+import 'package:bina_dokter/view/chatwithdoctor.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:bina_dokter/view/chatwithdoctor.dart'; // Tambahkan impor ini
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:fl_chart/fl_chart.dart';
 
 class Mainmenu extends StatefulWidget {
   const Mainmenu({super.key});
@@ -13,17 +16,20 @@ class Mainmenu extends StatefulWidget {
 }
 
 class _MainmenuState extends State<Mainmenu> {
+  int _selectedIndex = 0;
   String? fullname;
   final ApiService _apiService = ApiService();
   List<Map<String, dynamic>> prescriptions = [];
   bool isLoading = true;
-  String? doctorId; // Tambahkan variabel ini untuk menyimpan doctor_id
+  String? doctorId;
+  List<Article> articles = [];
 
   @override
   void initState() {
     super.initState();
     fetchfullname();
     fetchPrescriptions();
+    fetchArticles();
   }
 
   Future<void> fetchPrescriptions() async {
@@ -138,100 +144,228 @@ class _MainmenuState extends State<Mainmenu> {
     }
   }
 
+  Future<void> fetchArticles() async {
+    final response = await http.get(Uri.parse('https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=6f2612131a394c3aa62df46b3075a398'));
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      setState(() {
+        articles = (jsonData['articles'] as List).map((article) => Article.fromJson(article)).toList();
+      });
+    } else {
+      throw Exception('Failed to load articles');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
+        child: IndexedStack(
+          index: _selectedIndex,
+          children: [
+            _buildHomeScreen(),
+            _buildArticleScreen(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article),
+            label: 'Articles',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildHomeScreen() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+          ),
+          Row(
             children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
+              Image.asset(
+                'assets/images/logo.png',
+                height: 100,
+                width: 100,
               ),
-              Row(
-                children: [
-                  Image.asset(
-                    'assets/images/logo.png',
-                    height: 100,
-                    width: 100,
-                  ),
-                  Text(
-                    "Bina Dokter",
-                    style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    width: 80,
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.settings_outlined,
-                      size: 40,
-                    ),
-                    onPressed: () {
-                      _showSettingsDialog();
-                    },
-                  )
-                ],
+              Text(
+                "Bina Dokter",
+                style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    _getGreeting(DateTime.now().hour),
-                    style: GoogleFonts.poppins(
-                      color: Colors.black,
-                      fontSize: 14,
-                    ),
-                  ),
-                  Text(
-                    (fullname ?? ''),
-                    style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
+              const SizedBox(
+                width: 80,
               ),
-              const SizedBox(height: 20),
-              isLoading
-                  ? CircularProgressIndicator()
-                  : prescriptions.isEmpty
-                      ? Text('No Prescriptions', style: GoogleFonts.poppins())
-                      : Wrap(
-                          spacing: 20,
-                          runSpacing: 20,
-                          alignment: WrapAlignment.center,
-                          children: [
-                            ...prescriptions.map((prescription) {
-                              return _buildMenuButton(
-                                icon: Icons.medical_services,
-                                label:
-                                    'Prescription #${prescriptions.indexOf(prescription) + 1}',
-                                onTap: () => _navigateToPrescriptionDetails(
-                                    prescription['prescription_id'].toString()),
-                              );
-                            }).toList(),
-                            _buildMenuButton(
-                              icon: Icons.local_pharmacy,
-                              label: 'Pharmacy',
-                              onTap: _navigateToPharmacy,
-                            ),
-                            _buildMenuButton(
-                              icon: Icons.chat,
-                              label: 'Chat with Doctor',
-                              onTap: () => doctorId != null ? _navigateToChatWithDoctor() : null,
-                            ),
-                          ],
-                        ),
+              IconButton(
+                icon: const Icon(
+                  Icons.settings_outlined,
+                  size: 40,
+                ),
+                onPressed: () {
+                  _showSettingsDialog();
+                },
+              )
             ],
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 20,
+              ),
+              Text(
+                _getGreeting(DateTime.now().hour),
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                (fullname ?? ''),
+                style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          isLoading
+              ? CircularProgressIndicator()
+              : prescriptions.isEmpty
+                  ? Text('No Prescriptions', style: GoogleFonts.poppins())
+                  : Wrap(
+                      spacing: 20,
+                      runSpacing: 20,
+                      alignment: WrapAlignment.center,
+                      children: [
+                        ...prescriptions.map((prescription) {
+                          return _buildMenuButton(
+                            icon: Icons.medical_services,
+                            label:
+                                'Prescription #${prescriptions.indexOf(prescription) + 1}',
+                            onTap: () => _navigateToPrescriptionDetails(
+                                prescription['prescription_id'].toString()),
+                          );
+                        }).toList(),
+                        _buildMenuButton(
+                          icon: Icons.local_pharmacy,
+                          label: 'Pharmacy',
+                          onTap: _navigateToPharmacy,
+                        ),
+                        _buildMenuButton(
+                          icon: Icons.chat,
+                          label: 'Chat with Doctor',
+                          onTap: () => doctorId != null ? _navigateToChatWithDoctor() : null,
+                        ),
+                      ],
+                    ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildArticleScreen() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          Text(
+            'Health Articles',
+            style: GoogleFonts.poppins(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          _buildHealthChart(),
+          const SizedBox(height: 20),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(articles[index].title),
+                subtitle: Text(articles[index].description),
+                onTap: () {
+                  // Implementasi untuk membuka artikel lengkap
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHealthChart() {
+    return Container(
+      height: 300,
+      padding: EdgeInsets.all(16),
+      child: BarChart(
+        BarChartData(
+          alignment: BarChartAlignment.spaceAround,
+          maxY: 100,
+          barTouchData: BarTouchData(enabled: false),
+          titlesData: FlTitlesData(
+            show: true,
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                getTitlesWidget: (double value, TitleMeta meta) {
+                  const style = TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14);
+                  String text;
+                  switch (value.toInt()) {
+                    case 0:
+                      text = 'Exercise';
+                      break;
+                    case 1:
+                      text = 'Diet';
+                      break;
+                    case 2:
+                      text = 'Sleep';
+                      break;
+                    case 3:
+                      text = 'Stress';
+                      break;
+                    default:
+                      text = '';
+                      break;
+                  }
+                  return Text(text, style: style);
+                },
+              ),
+            ),
+          ),
+          borderData: FlBorderData(show: false),
+          barGroups: [
+            BarChartGroupData(x: 0, barRods: [BarChartRodData(toY: 75, color: Colors.blue)]),
+            BarChartGroupData(x: 1, barRods: [BarChartRodData(toY: 60, color: Colors.green)]),
+            BarChartGroupData(x: 2, barRods: [BarChartRodData(toY: 80, color: Colors.purple)]),
+            BarChartGroupData(x: 3, barRods: [BarChartRodData(toY: 40, color: Colors.orange)]),
+          ],
         ),
       ),
     );
@@ -290,6 +424,20 @@ class _MainmenuState extends State<Mainmenu> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class Article {
+  final String title;
+  final String description;
+
+  Article({required this.title, required this.description});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
     );
   }
 }
